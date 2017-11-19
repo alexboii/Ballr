9e7b8dfd0e0ed1a31afe40d6306ed60734d5e8e3
erick <erick@hotmail.ca>
2017-11-18 19:08:30 -0500

    Add cute team logo to halfcourt

--- app/components/Court.js
+++ app/components/Court.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Rect, Layer, Stage } from 'react-konva';
 import PropTypes from 'prop-types';
 import Player from './Player';
+import { TEAM_LOGO } from '../constants/ImageConstants';
 
 class Court extends Component {
 
@@ -15,6 +16,8 @@ class Court extends Component {
     const players = this.props.selectedPlayers.map(
       data => <Player key={data.player_id} courtX={750} courtY={704} radius={22} data={data} />,
     );
+    console.log(this.props.selectedPlayers);
+    const teamName = (this.props.selectedPlayers.length > 0 && this.props.selectedPlayers[0]['player_stats.team']) || 'gsw';
     return (
       <Stage width={750} height={704}>
         <Layer>
@@ -22,6 +25,14 @@ class Court extends Component {
             fillPatternImage={this.createImage('https://i.imgur.com/8XqqzWp.png')}
             width={750}
             height={704}
+          />
+          <Rect
+            fillPatternImage={this.createImage(`${TEAM_LOGO}${teamName}.png`)}
+            width={500}
+            height={500}
+            rotate={Math.PI / 2}
+            x={125}
+            y={-250}
           />
           {players}
         </Layer>
